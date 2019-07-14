3e3665b03dcb6ce3b195e84b6f35cc951b6151d2
Nikita-Mamaisur <52109683+Nikita-Mamaisur@users.noreply.github.com>
2019-07-15 02:11:27 +0300

    Add files via upload

---
+++ Lawyer/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Lawyer.App_Start
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new StyleBundle("~/content/lib")
+			   .Include("~/content/bootstrap.min.css")
+			   .Include("~/content/summernote-bs4.css"));
+
+			bundles.Add(new ScriptBundle("~/scripts/lib")
+				.Include("~/scripts/jquery-3.4.1.min.js")
+				.Include("~/scripts/umd/popper.min.js")
+				.Include("~/scripts/bootstrap.min.js")
+				.Include("~/scripts/summernote/summernote-bs4.min.js"));
+		}
+	}
+}
